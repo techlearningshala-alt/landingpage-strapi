4741c90f59578e73b0aed90206da9bd20c81be4f
MIPL <mipl@MIPLs-MacBook-Air.local>
2025-11-11 12:20:48 +0530

    fix

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,23 @@
 export default [
   'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'http://localhost:3000',
+        'https://edusprinthub.com',
+        'https://www.edusprinthub.com',
+        'https://studybettertoday.com',
+        'https://www.studybettertoday.com',
+      ],
+      headers: ['*'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
